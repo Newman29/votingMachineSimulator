3cbe707c3b1d17e804a192d3f57919bfe336e604
ryann <ryann@octitle.com>
2016-02-09 11:36:28 -0800

    Added Democratic and Republican nominees

--- app/src/main/java/com/newman/ryan/votingmachinesimullator/MainActivity.java
+++ app/src/main/java/com/newman/ryan/votingmachinesimullator/MainActivity.java
@@ -4,6 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] republicanCandidates = {
+        "Jeb Bush",
+        "Ben Carson",
+        "Chris Christie",
+        "Ted Cruz",
+        "Carly Fiorina",
+        "John Kasich",
+        "Marco Rubio",
+        "Donald Trump"
+    };
+
+    private String[] democratCandidates = {
+        "Bernie Sanders",
+        "Hillary Clinton"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
