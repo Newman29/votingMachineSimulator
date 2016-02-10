0ed0c190da21299016e5b0465fec7f4464d8b404
ryann <ryann@octitle.com>
2016-02-09 16:39:46 -0800

    Added a check for root which would enable the app unsecure to be on the device. Check on a nonrooted device and it worked. Need to check on rooted devices

--- app/src/main/java/com/newman/ryan/votingmachinesimullator/MainActivity.java
+++ app/src/main/java/com/newman/ryan/votingmachinesimullator/MainActivity.java
@@ -1,5 +1,8 @@
 package com.newman.ryan.votingmachinesimullator;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -11,21 +14,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
     private String[] republicanCandidates = {
-        "Jeb Bush",
-        "Ben Carson",
-        "Chris Christie",
-        "Ted Cruz",
-        "Carly Fiorina",
-        "John Kasich",
-        "Marco Rubio",
-        "Donald Trump"
+            "Jeb Bush",
+            "Ben Carson",
+            "Chris Christie",
+            "Ted Cruz",
+            "Carly Fiorina",
+            "John Kasich",
+            "Marco Rubio",
+            "Donald Trump"
     };
 
     private String[] democratCandidates = {
-        "Bernie Sanders",
-        "Hillary Clinton"
+            "Bernie Sanders",
+            "Hillary Clinton"
     };
 
     //Declare the SharedPreferences and Editor
@@ -44,18 +49,33 @@ public class MainActivity extends AppCompatActivity {
         //set the flag to false while development for debugging purposes
         prefs.edit().putBoolean("firstTimeUsing", true).apply();
 
-        //Try and detect if this is the users first time launching the app
-        if (prefs.getBoolean("firstTimeUsing", true)) {
-            Log.d("First time launching: ", "yes");
-
-            addCandidates(democratCandidates);
-            addCandidates(republicanCandidates);
-
-            createPasswordDialog();
-
-            prefs.edit().putBoolean("firstTimeUsing", false).apply();
+        if (isRooted()) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Unsecure Phone")
+                    .setMessage("We detect that this device is rooted, enabling it to be unsecure for this application. If you feel like this is a mistake, feel free to contact the developer. The application will now be uninstalled")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // continue with delete
+                            uninstallApp();
+                            finish();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         } else {
-            Log.d("First time launching: ", "no");
+            //Try and detect if this is the users first time launching the app
+            if (prefs.getBoolean("firstTimeUsing", true)) {
+                Log.d("First time launching: ", "yes");
+
+                addCandidates(democratCandidates);
+                addCandidates(republicanCandidates);
+
+                createPasswordDialog();
+
+                prefs.edit().putBoolean("firstTimeUsing", false).apply();
+            } else {
+                Log.d("First time launching: ", "no");
+            }
         }
     }
 
@@ -63,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
     Add the candidates to SharedPreferences with the key being their name with no spaces and
     an initial value of 0. This will be changed with each vote for that candidate by one
      */
-    private void addCandidates (String[] candidates) {
+    private void addCandidates(String[] candidates) {
         for (String candidate : candidates) {
             boolean hasCandidate = prefs.contains(candidate.replace(" ", ""));
 
@@ -90,7 +110,44 @@ public class MainActivity extends AppCompatActivity {
                 // handle button click
                 EditText passwordInput = (EditText) passwordDialog.findViewById(R.id.edit_adminPassword);
                 String password = passwordInput.getText().toString();
+
+
+
+                passwordDialog.dismiss();
             }
         });
+    }
+
+    private void uninstallApp() {
+        Intent intent = new Intent(Intent.ACTION_DELETE);
+        intent.setData(Uri.parse("package:com.newman.ryan.votingmachinesimullator"));
+        startActivity(intent);
+    }
+
+    /**
+     * Checks if the device is rooted.
+     * <p>
+     * http://stackoverflow.com/questions/19288463/how-to-check-if-android-phone-is-rooted
+     *
+     * @return true if device is rooted, otherwise false
+     */
+    private static boolean isRooted() {
+        return findBinary("su");
+    }
+
+    public static boolean findBinary(String binaryName) {
+        boolean found = false;
+
+        String[] places = {"/sbin/", "/system/bin/", "/system/xbin/", "/data/local/xbin/",
+                "/data/local/bin/", "/system/sd/xbin/", "/system/bin/failsafe/", "/data/local/"};
+
+        for (String where : places) {
+            if (new File(where + binaryName).exists()) {
+                found = true;
+                break;
+            }
+        }
+
+        return found;
     }
 }
